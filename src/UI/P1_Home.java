ab659277b0a4b2840ea14d0795800e0abb2db4c2
tushar <iamtusharpuri@gmail.com>
2023-05-31 15:09:34 +0530

    UI of login page and main page

--- src/UI/P1_Home.java
+++ src/UI/P1_Home.java
@@ -1,17 +1,39 @@
 package UI;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class P1_Home extends JPanel {
     public P1_Home() {
         setLayout(new BorderLayout());
         setBackground(Color.WHITE);
 
-        JPanel titlePanel = new JPanel();
-        titlePanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // Set the padding
+        JPanel titlePanel = new JPanel(new BorderLayout());
+        titlePanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         titlePanel.setBackground(Color.WHITE);
-        titlePanel.setLayout(new BorderLayout()); // Set layout to BorderLayout
+
+        // Create the logout button
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.setFont(new Font("Arial", Font.PLAIN, 12));
+        logoutButton.setFocusPainted(false);
+        logoutButton.setPreferredSize(new Dimension(140, 25));
+
+        // Add action listener to the logout button
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Perform logout action here
+                // ...
+
+                // Open the login page again
+                LoginPage loginPage = new LoginPage();
+                loginPage.setVisible(true);
+                SwingUtilities.getWindowAncestor(P1_Home.this).dispose();
+            }
+        });
 
         JLabel titleLabel = new JLabel("Welcome"); // TODO: Add name of the employee here
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
@@ -22,10 +44,15 @@ public class P1_Home extends JPanel {
         textLabel.setFont(new Font("Arial", Font.ITALIC, 18));
         textLabel.setHorizontalAlignment(SwingConstants.CENTER); // Align label to the center
 
-        titlePanel.add(titleLabel, BorderLayout.NORTH); // Add titleLabel to the top
-        titlePanel.add(Box.createRigidArea(new Dimension(0, 45)), BorderLayout.CENTER);
-        titlePanel.add(textLabel, BorderLayout.SOUTH); // Add textLabel to the center
+        JPanel logoutPanel = new JPanel();
+        logoutPanel.setOpaque(false);
+        logoutPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        logoutPanel.add(logoutButton);
+
+        titlePanel.add(logoutPanel, BorderLayout.NORTH);
+        titlePanel.add(titleLabel, BorderLayout.CENTER);
+        titlePanel.add(textLabel, BorderLayout.SOUTH);
+
         add(titlePanel, BorderLayout.NORTH);
     }
 }
-
